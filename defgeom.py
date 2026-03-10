a287ae8bc5dd4deab18a12168c67ad8a59cd8e2c
Panos Sachinis <pansachinis@gmail.com>
2026-03-10 11:24:59 +0100

    yippee

--- defgeom.py
+++ defgeom.py
@@ -1,7 +1,30 @@
 import numpy as np
 
-def defGeom(R, N, start, twist, pitch, cdist):
+# x = np.linspace(0.25,1,100)
 
-    c = R*cdist
+# R = 0.7
+# B = 6
+# start = 0.25
+# twist = -np.radians(50)*x+np.radians(35) # for x>0.25
+# pitch = np.radians(46) # x=0.7
+# cdist = 0.18-0.06*x # for x>0.25
 
-    return
+def defGeom(R, B, x, start, tdist, pitch, cdist):
+
+    r = x*R
+
+    c = R*cdist                 # Local Chord
+    theta = tdist+pitch         # Local Pitch Angle
+    sigma = B*c/(2*np.pi*r)     # Local Solidity
+
+    return c, theta, sigma
+
+# c, theta, sigma = defGeom(R,B,x,start,twist,pitch,cdist)
+
+# import matplotlib.pyplot as plt
+# plt.plot(x,c)
+# plt.show()
+# plt.plot(x,np.degrees(theta))
+# plt.show()
+# plt.plot(x,sigma)
+# plt.show()
--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import defgeom
+import calcloads
+import getforces
+import momcorrections
 
 ### Geometry Configuration
 
@@ -8,7 +12,7 @@ x = np.linspace(0.25,1,100)
 R = 0.7
 B = 6
 start = 0.25
-twist = -np.radians(50)*x+np.radians(35) # for x>0.25
+tdist = -np.radians(50)*x+np.radians(35) # for x>0.25
 pitch = np.radians(46) # x=0.7
 cdist = 0.18-0.06*x # for x>0.25
 
@@ -21,3 +25,21 @@ RPM = 1200
 h = 2000
 fi = 0
 ry = 0
+
+maxiter = 101
+
+### Calculate geometry specification
+
+c, theta, sigma = defgeom.defGeom(R,B,x,start,tdist,pitch,cdist)
+
+### Obtain forces
+
+
+
+### Main Iteration Loop
+
+iter=0
+while iter<maxiter:
+
+    temp=0
+    iter+=1
